f391098fda3387fdb03cd3dc608ef2b62dcc9d80
type-y-stimpack <flopernikl@gmail.com>
2025-01-15 09:37:55 +0100

    Square and Circle
    Honestly actually forget all my nonsense with gitattributes because it's kind of embarrassing
    Anyway this just adds a square and a circle, committing this mostly since Unity is acting a bit off

---
+++ Click Frenzy/Assets/Scripts/Circle.cs
@@ -0,0 +1,26 @@
+    using System;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class Circle : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnMouseOver()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+        }
+    }
